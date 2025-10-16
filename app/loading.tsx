5103957e6a8e4f98a133e1f3a666043a0a0b44db
Chloé Adrian <chloe.adrian@abtasty.com>
2025-10-16 12:47:04 +0200

    Add loader

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import styles from "./loading.module.scss";
+
+const Loading = () => {
+    return (
+        <main className={styles.loading}>
+            <span></span>
+        </main>
+    )
+}
+
+export default Loading;
